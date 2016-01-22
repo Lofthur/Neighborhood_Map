4e5a86c7b5116d1c31c449abad5ed093ca71ba6f
Stein-Terje Dale <Stein-Terje Dale>
2016-01-22 14:22:49 +0100

    added adress and web info to places objects

--- js/places.js
+++ js/places.js
@@ -5,37 +5,55 @@ var Places = function() {
 			title: 'Kilden',
 			lat: 58.13937575197785,
 			lng: 7.996823787689208,
-			info: 'This is the cultural arena in the southern part of norway.'
+			adr: 'Sjølystveien 2,4610 Kristiansand',
+			web: 'www.kilden.com',
+			info: 'This is the cultural arena in the southern part of norway.',
+			filter: 'culture'
 		},
 		{
 			title: 'Kino',
 			lat: 58.141621,
 			lng: 7.995329,
-			info: 'Here we get to see all the great movies.'
+			adr: 'Vestre Strandgate 9,4610 Kristiansand',
+			web: 'www.filmweb.no',
+			info: 'Here we get to see all the great movies.',
+			filter: 'culture'
 		},
 		{
 			title: 'Dampbageriet',
 			lat: 58.145111,
 			lng: 7.993475,
-			info: 'The place to get the best sweet buns.'
+			adr: 'Rådhusgata 5,4611 Kristiansand',
+			web: 'www.dampbageriet.no',
+			info: 'The place to get the best sweet buns.',
+			filter: 'cafe'
 		},
 		{
 			title: 'Drømmeplassen',
 			lat: 58.14712596928417,
 			lng: 7.993608408907503,
-			info: 'A good cafe in Kristiansand.'
+			adr: 'Skippergata 26,4611 Kristiansand',
+			web: 'www.drommeplassen.no',
+			info: 'A good cafe in Kristiansand.',
+			filter: 'cafe'
 		},
 		{
-		title: 'Dyreparken',
-		lat: 58.182893,
-		lng: 8.148460,
-		info: 'The largest, and greatest Zoo in Norway'
+			title: 'Dyreparken',
+			lat: 58.182893,
+			lng: 8.148460,
+			adr: 'Kardemomme By 4609,Kristiansand',
+			web: 'www.dyreparken.no',
+			info: 'The largest, and greatest Zoo in Norway',
+			filter: 'zoo'
 		},
 		{
 			title: 'UIA',
 			lat: 58.163210,
 			lng: 8.003229,
-			info: 'The university in the southern part of Norway.'
+			adr: 'Gimlemoen 25,4630 Kristiansand',
+			web: 'www.uia.no',
+			info: 'The university in the southern part of Norway.',
+			filter: 'education'
 		}
 	]
 
