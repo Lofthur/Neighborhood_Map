82206dfbf10557cb49f433aace9ea4d543b822d3
Stein-Terje Dale <Stein-Terje Dale>
2016-02-03 21:25:38 +0100

    Add filter subscription for My Places, list click for FS table

--- js/app.js
+++ js/app.js
@@ -1,21 +1,110 @@
-'use strict';
+'use strict'
 
 var NMap = function() {
 
 	var MapModel = {
-		searchText:  ko.observable(),
+		searchText:  ko.observable(''),
+		filterText: ko.observable(''),
 		markerArray: ko.observableArray([]),
 		placesArray: ko.observableArray([]),
 		holdMarkersArray: ko.observableArray([]),
+		filterArray: ko.observableArray([]),
+		holdFilterMarkers: ko.observableArray([]),
 		fourSquareInfoArray: ko.observableArray([]),
 		holdFourSquareMarkersArray: ko.observableArray([]),
-		isError: ko.observable()
+		isError: ko.observable(),
 	};
 
 	var map;
 	var places = Places();
 	var openInfoWindow;
 	var fourSquareInfoWindow;
+	var bufferFilterMarkers = ko.observableArray([]);
+	var isDelete = false;
+	var isBuffered = false;
+
+	var tempString;
+	MapModel.filterText.subscribe(function(newValue) {
+	var arrLengthFilter = MapModel.placesArray().length;
+
+		if(isDelete == false) {
+			for(var i = 0; i < MapModel.holdFourSquareMarkersArray().length; i++) {
+				MapModel.holdFourSquareMarkersArray()[i].setMap(null);
+			}
+			MapModel.fourSquareInfoArray.removeAll();
+			isDelete = true;
+		}
+
+			filterDelete();
+			MapModel.markerArray.removeAll();
+
+			for(var i = 0; i < MapModel.holdMarkersArray().length; i++) {
+				MapModel.holdMarkersArray()[i].setMap(null);
+			}
+			MapModel.holdMarkersArray.removeAll();
+
+		for(var i = 0; i < arrLengthFilter; i++) {
+			tempString = MapModel.placesArray()[i].title.toUpperCase();
+
+			if(tempString.search(MapModel.filterText().toUpperCase()) > -1) {
+				MapModel.markerArray.push(MapModel.placesArray()[i]);
+				MapModel.holdFilterMarkers.push(bufferFilterMarkers()[i]);
+			}
+		}
+		if(MapModel.filterText() == '') {
+			MapModel.markerArray.removeAll();
+			MapModel.holdMarkersArray.removeAll();//?????
+
+			for(var i = 0; i < MapModel.placesArray().length; i++) {
+				MapModel.markerArray.push(MapModel.placesArray()[i]);
+			}
+
+			setMarkers(MapModel.markerArray(), MapModel.holdMarkersArray());
+			isDelete = false;
+		}
+		setMarkers(MapModel.markerArray(), MapModel.holdMarkersArray());
+	});
+
+	/*MapModel.filterText.subscribe(function(newValue) {
+	var arrLengthFilter = MapModel.placesArray().length;
+	console.log(isDelete);
+		if(isDelete == false) {
+			for(var i = 0; i < MapModel.holdMarkersArray().length; i++) {
+				MapModel.holdMarkersArray()[i].setMap(null);
+			}
+			for(var i = 0; i < MapModel.holdFourSquareMarkersArray().length; i++) {
+				MapModel.holdFourSquareMarkersArray()[i].setMap(null);
+			}
+			MapModel.fourSquareInfoArray.removeAll();
+			isDelete = true;
+		}
+
+			filterDelete();
+			MapModel.markerArray.removeAll();
+
+		for(var i = 0; i < arrLengthFilter; i++) {
+			tempString = MapModel.placesArray()[i].title.toUpperCase();
+
+			if(tempString.search(MapModel.filterText().toUpperCase()) > -1) {
+				MapModel.filterArray.push(MapModel.placesArray()[i]);
+				MapModel.markerArray.push(MapModel.placesArray()[i]);
+				MapModel.holdFilterMarkers.push(bufferFilterMarkers()[i]);
+			}
+		}
+		if(MapModel.filterText() == '') {
+			filterDelete();
+			MapModel.markerArray.removeAll();
+
+			for(var i = 0; i < MapModel.placesArray().length; i++) {
+				MapModel.markerArray.push(MapModel.placesArray()[i]);
+			}
+
+			setMarkers(MapModel.markerArray(), MapModel.holdMarkersArray());
+			isDelete = false;
+		}
+
+		setFilterMarkers();
+	});*/
 
 	// Initializes the arrays with data from places
 	var initArrays = function() {
@@ -27,7 +116,7 @@ var NMap = function() {
 	};
 
 	// Initializes google map
-	var initMap = function() {
+	function initMap() {
 		var mapProp = {
 			center: new google.maps.LatLng(58.144744, 7.994747),
 			zoom: 13,
@@ -36,10 +125,14 @@ var NMap = function() {
 		};
 		var nMap = new google.maps.Map(document.getElementById('map'), mapProp);
 		map = nMap;
+	}
+
+	var mapError = function() {
+		alert('Could not load Google Maps');
 	};
 
 	// This function creates the markers from the markerArray
-	var setMarkers = function() {
+	/*var setMarkers = function() {
 		var arrLength = MapModel.markerArray().length;
 		var initLat;
 		var marker;
@@ -58,7 +151,7 @@ var NMap = function() {
 			});
 
 			infoWindow = new google.maps.InfoWindow({
-				content: '<h2>' + MapModel.markerArray()[i].title + '</h2>' + '<br/>' +  '<p>' + MapModel.markerArray()[i].adr + '</p>' + '</br>' + '<a href="http://'+ MapModel.markerArray()[i].web+ '" target="_blank">' + MapModel.markerArray()[i].web + '</a>' + '</p>' + '</br>' + '<p>' + MapModel.markerArray()[i].info + '</p>'
+				content: '<h2>' + MapModel.markerArray()[i].title + '</h2>' + '<br/>' +  '<p>' + MapModel.markerArray()[i].adr + '</p>' + '</br>' + '<a href="http://'+ MapModel.markerArray()[i].web + '" target="_blank">' + MapModel.markerArray()[i].web + '</a>' + '</p>' + '</br>' + '<p>' + MapModel.markerArray()[i].info + '</p>'
 			});
 
 			(function(infoWindow, marker) {
@@ -80,6 +173,95 @@ var NMap = function() {
 			MapModel.holdMarkersArray.push(marker);
 			marker.setMap(map);
 		}
+	};*/
+
+	/*var setFilterMarkers = function() {
+		var arrLength = MapModel.filterArray().length;
+		var initLat;
+		var marker;
+		var infoWindow;
+		var initLng;
+		var latLng;
+
+		for(var i = 0; i < arrLength; i++) {
+			initLat = MapModel.filterArray()[i].lat;
+			initLng = MapModel.filterArray()[i].lng;
+			latLng = {lat: initLat, lng: initLng};
+
+			marker = new google.maps.Marker({
+				position: latLng,
+			});
+
+			infoWindow = new google.maps.InfoWindow({
+				content: '<h2>' + MapModel.filterArray()[i].title + '</h2>' + '<br/>' +  '<p>' + MapModel.filterArray()[i].adr + '</p>' + '</br>' + '<a href="http://'+ MapModel.filterArray()[i].web + '" target="_blank">' + MapModel.filterArray()[i].web + '</a>' + '</p>' + '</br>' + '<p>' + MapModel.filterArray()[i].info + '</p>'
+			});
+
+			(function(infoWindow, marker) {
+				marker.addListener('click', function() {
+
+					closeLastOpenInfoWindow(openInfoWindow);
+					openInfoWindow = infoWindow;
+					infoWindow.open(map, marker);
+					if(marker.getAnimation() != null) {
+						marker.setAnimation() == null;
+					} else {
+						marker.setAnimation(google.maps.Animation.BOUNCE);
+						setTimeout(function(){marker.setAnimation(null);}, 1400);
+					}
+
+				});
+			}(infoWindow, marker));
+
+			MapModel.holdFilterMarkers.push(marker);
+			marker.setMap(map);
+		}
+	};*/
+
+		var setMarkers = function(marker, holdMarker) {
+		var mArr = ko.observableArray(marker);
+		var arrLength = mArr().length;
+		var initLat;
+		var marker;
+		var infoWindow;
+		var initLng;
+		var latLng;
+
+		for(var i = 0; i < arrLength; i++) {
+			initLat = mArr()[i].lat;
+			initLng = mArr()[i].lng;
+			latLng = {lat: initLat, lng: initLng};
+
+			marker = new google.maps.Marker({
+				position: latLng,
+			});
+
+			infoWindow = new google.maps.InfoWindow({
+				content: '<h2>' + mArr()[i].title + '</h2>' + '<br/>' +  '<p>' + mArr()[i].adr + '</p>' + '</br>' + '<a href="http://'+ mArr()[i].web + '" target="_blank">' + mArr()[i].web + '</a>' + '</p>' + '</br>' + '<p>' + mArr()[i].info + '</p>'
+			});
+
+			(function(infoWindow, marker) {
+				marker.addListener('click', function() {
+
+					closeLastOpenInfoWindow(openInfoWindow);
+					openInfoWindow = infoWindow;
+					infoWindow.open(map, marker);
+					if(marker.getAnimation() != null) {
+						marker.setAnimation() == null;
+					} else {
+						marker.setAnimation(google.maps.Animation.BOUNCE);
+						setTimeout(function(){marker.setAnimation(null);}, 1400);
+					}
+
+				});
+			}(infoWindow, marker));
+			if(isBuffered == false) {
+				bufferFilterMarkers.push(marker);
+			}
+			holdMarker.push(marker);
+			marker.setMap(map);
+		}
+		console.log(isBuffered);
+		isBuffered = true;
 	};
 
 	var closeLastOpenInfoWindow = function(window) {
@@ -94,54 +276,65 @@ var NMap = function() {
 	// If there is a match it will be pushed to markerArray.
 	// The setMarkers and getFourSquareInfo is called
 	var search = function() {
-		var arrLength = MapModel.placesArray().length;
+		deleteMarkers();
 
-		deleteMarkers(MapModel.markerArray(), MapModel.holdMarkersArray());
-		for(var i = 0; i < arrLength; i++) {
-			if(MapModel.searchText().toUpperCase() == MapModel.placesArray()[i].title.toUpperCase()) {
-				MapModel.markerArray.push(MapModel.placesArray()[i]);
-			}
-		}
-		setMarkers();
 		getFourSquareInfo(MapModel.searchText());
 	};
 
-	// Deletes the information that is inside markerArray
-	// and holdMarkerArray
-	var deleteMarkers = function() {
-		var arrLength = MapModel.markerArray().length;
+
+	function deleteMarkers() {
+		var arrLength = MapModel.holdMarkersArray().length;
+			for(var i = 0; i < arrLength; i++) {
+				MapModel.holdMarkersArray()[i].setMap(null);
+			}
+
+		//MapModel.markerArray.removeAll();
+		//MapModel.holdMarkersArray.removeAll();
+	}
+
+	var filterDelete = function() {
+		var arrLength = MapModel.holdFilterMarkers().length;
 
 		for(var i = 0; i < arrLength; i++) {
-			MapModel.holdMarkersArray()[i].setMap(null);
+			MapModel.holdFilterMarkers()[i].setMap(null);
 		}
-		MapModel.markerArray.removeAll();
-		MapModel.holdMarkersArray.removeAll();
+
+		MapModel.filterArray.removeAll();
+		MapModel.holdFilterMarkers.removeAll();
 	};
 
 	// Gets the item that is clicked in the array.
 	// Goes through the markerArray, and checks if the data.title
 	// is equal to a title in the markerArray.
 	// If it is, index is set to the i value of the array
-	// linkClick is called with the index value
-	var openInfo = function(data) {
+	// linkClick is called with the ndex value
+	var openInfo = function(data) {i
 		var arrLength = MapModel.markerArray().length;
 		var index;
+
 		for(var i = 0; i < arrLength; i++) {
 			if(data.title == MapModel.markerArray()[i].title){
 				index = i;
 			}
 		}
-		linkClick(index);
+		linkClick(index, MapModel.holdMarkersArray());
 	};
+
 	// Triggers the marker click event from a table cell
-	var linkClick = function(id) {
-		google.maps.event.trigger(MapModel.holdMarkersArray()[id], 'click');
+	var linkClick = function(id, arrayOfMarkers) {
+		var mArr = ko.observableArray(arrayOfMarkers);
+		google.maps.event.trigger(mArr()[id], 'click');
 	};
+
+	/*var linkClick = function(id) {
+		google.maps.event.trigger(MapModel.holdMarkersArray()[id], 'click');
+	};*/
 
 	// This function uses the Four Square API
 	// uses $getJSon to get information fron Four Square
 	var getFourSquareInfo = function(searhQuery) {
 		var venues;
+
 		var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?client_id=RU3QTW3F1CUFGAZQM5KGLZSJHV5BVIBY0XDXODIED2CKR4BL&client_secret=YB04C05RQWLCRL2Y1U1KFZKGHKSCUSQ10SZ5VLRHRKJS0UCS&v=20130815&ll=58,8&ll=58.144744, 7.994747&radius=1500&query=' + searhQuery + '';
 		var fourSquareInfoWindow;
 
@@ -149,6 +342,7 @@ var NMap = function() {
 
 		$.getJSON(fourSquareURL, function(data) {
 			venues = data.response.venues;
+			console.log(venues);
 			for(var i = 0; i < venues.length; i++) {
 				MapModel.fourSquareInfoArray.push(venues[i]);
 			}
@@ -179,7 +373,7 @@ var NMap = function() {
 		for(var i = 0; i < arrLength; i++) {
 			lat = MapModel.fourSquareInfoArray()[i].location.lat;
 			lng = MapModel.fourSquareInfoArray()[i].location.lng;
-			markerInfo = MapModel.fourSquareInfoArray()[i].name;
+			markerInfo = '<h2>' + MapModel.fourSquareInfoArray()[i].name + '</h2>';
 			latLng = {lat: lat, lng: lng};
 
 			marker = new google.maps.Marker({
@@ -221,22 +415,34 @@ var NMap = function() {
 				MapModel.holdFourSquareMarkersArray.removeAll();
 	};
 
+	var openFSInfo = function(data) {
+		var index;
+		var arrLength = MapModel.fourSquareInfoArray().length;
+
+		for(var i = 0; i < arrLength; i++) {
+			if(data.name == MapModel.fourSquareInfoArray()[i].name) {
+				index = i;
+			}
+		}
+		linkClick(index, MapModel.holdFourSquareMarkersArray());
+	};
+
 	// Calls the functions that are necessary for the application to run
 	var init = function() {
 		initMap();
 		initArrays();
-		setMarkers();
+		setMarkers(MapModel.markerArray(), MapModel.holdMarkersArray());
 		ko.applyBindings(MapModel);
 	};
-
-	// Calls the init function when the document is ready
-	$(init);
 
 	// Set the returned content as public
 	return {
 		MapModel: MapModel,
 		search: search,
-		openInfo: openInfo
+		openInfo: openInfo,
+		openFSInfo: openFSInfo,
+		mapError: mapError,
+		init: init,
 	};
 
 }();
